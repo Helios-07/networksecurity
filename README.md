91bc43402c1f974512ef6a741f6109644cf361cf
Aman Nautiyal <amannautiyal794@gmail.com>
2025-07-22 02:11:59 +0530

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 ### Network Security Project for Phising Data
+
+### 📦 Dataset
+📦 Dataset
+The core dataset is stored in a MongoDB Atlas database. An ETL (Extract, Transform, Load) pipeline is used to process and load the data, which contains various features extracted from network traffic to be used for model training.
+![Mongodb_Atlas_Collection_View](screenshots/mongodb_atlas_collection_view.png)
+
+
+---
+
+### 📊 Experiment Tracking & Versioning
+We use MLflow for tracking experiments and DagsHub for versioning our code and data, and for comparing results.
+
+## Single Experiment Run
+Here is a view of a single experiment's details and metrics tracked in MLflow.
+![Mlflow_Single_Run_Metrics](screenshots/mlflow_single_run_metrics.png)
+
+
+## Comparing Experiments
+DagsHub provides a powerful interface to compare different experiment runs side-by-side, helping us select the best-performing model.
+![Dagshub_Experiment_Comparison](screenshots/dagshub_experiment_comparison.png)
--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -9,6 +9,8 @@ from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.components.model_trainer import ModelTrainer
 
+
+
 from networksecurity.entity.config_entity import (
     TrainingPipelineConfig,
     DataIngestionConfig,
@@ -79,6 +81,8 @@ class TrainingPipeline:
         except Exception as e:
             raise NetworkSecurityException(e, sys)
 
+
+
     def run_pipeline(self) -> ModelTrainerArtifact:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
@@ -86,6 +90,7 @@ class TrainingPipeline:
             data_transformation_artifact = self.start_data_transformation(
                 data_validation_artifact=data_validation_artifact)
             model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+
             return model_trainer_artifact
         except Exception as e:
             raise NetworkSecurityException(e, sys)
